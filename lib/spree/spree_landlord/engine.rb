a98712c3a37bca391e586ce3a048fdb8bc0cb9fd
M. Scott Ford <scott@mscottford.com>
2012-12-03 13:54:02 -0500

    Corrects path to load decorators

--- lib/spree/spree_landlord/engine.rb
+++ lib/spree/spree_landlord/engine.rb
@@ -13,7 +13,7 @@ module Spree
       end
 
       def self.activate
-        Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
+        Dir.glob(File.join(File.dirname(__FILE__), '../../../app/**/*_decorator*.rb')) do |c|
           Rails.configuration.cache_classes ? require(c) : load(c)
         end
         Spree::Ability.register_ability(Spree::TenantAbility)
