928749f300a943ec2f79784e01c51cf84ab5aeaa
M. Scott Ford <scott@mscottford.com>
2012-12-06 15:33:46 -0500

    Prevents exceptions when migrating an existing app
    Exceptions were being thrown when `rake db:migrate` was run while
    installing the extension into a project with an existing database.
    
    This commit ensures that ActiveRecord won't attempt to access the
    tenant_id field or the spree_tenants table until they have been
    created by migrations.

--- app/models/spree/shipping_method_decorator.rb
+++ app/models/spree/shipping_method_decorator.rb
@@ -1,6 +1,10 @@
 module Spree
   ShippingMethod.class_eval do
     # We need to redefine the default_scope to include the tenant_id
-    default_scope lambda { where( tenant_id: Tenant.current_tenant_id, deleted_at: nil ) }
+    default_scope lambda {
+      if ShippingMethod.column_names.include?(:tenant_id)
+        where( tenant_id: Tenant.current_tenant_id, deleted_at: nil )
+      end
+    }
   end
 end
--- app/models/spree/tax_category_decorator.rb
+++ app/models/spree/tax_category_decorator.rb
@@ -1,6 +1,10 @@
 module Spree
   TaxCategory.class_eval do
     # We need to redefine the default_scope to include the tenant_id
-    default_scope lambda { where( tenant_id: Tenant.current_tenant_id, deleted_at: nil ) }
+    default_scope lambda {
+      if TaxCategory.column_names.include?(:tenant_id)
+        where( tenant_id: Tenant.current_tenant_id, deleted_at: nil )
+      end
+    }
   end
 end
