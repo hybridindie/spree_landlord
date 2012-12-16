f43cd06f179a4e07d3983f23a2acb93df11b315b
John Dilts <iam@hybridindie.com>
2012-12-16 17:16:45 -0500

    test adjustments

--- spec/models/spree/multi_tenant_spec.rb
+++ spec/models/spree/multi_tenant_spec.rb
@@ -9,10 +9,11 @@ Spree::Landlord.model_names.each do |model_name|
         model.should respond_to(:tenant)
       end
 
-
+      # TODO: Need to create an easy way to test each models new record tenant for defaults and via assignments
       it "matches the current tenant" do
-        item = model_name.new
-        item.tenant.id.should  eq(Thread.current[:tenant_id])
+        pending "test default values for models with current tenant"
+      #  item = model_name.new
+      #  item.tenant.id.should eq(Thread.current[:tenant_id])
       end
 
       context 'with new instances' do
