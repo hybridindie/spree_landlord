3d1c807cd3e7072bdb693b0c50181a30a27d2fd0
John Dilts <iam@hybridindie.com>
2012-12-17 11:55:51 -0500

    shifts of where assets pathing and view pathing are taking place

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,29 +1,5 @@
 module Spree
   BaseController.class_eval do
 
-    before_filter :tenant_scope
-
-    private
-
-    def tenant_scope
-      tenant = Tenant.current_tenant
-
-      raise 'DomainUnknown' unless tenant
-
-      Proc.new {
-        # Add tenant views path
-        path = "app/tenants/#{tenant.shortname}/views"
-        prepend_view_path(path)
-
-        asset_paths = ["app/tenants/#{tenant.shortname}/assets/images",
-                       "app/tenants/#{tenant.shortname}/assets/stylesheets",
-                       "app/tenants/#{tenant.shortname}/assets/javascript"]
-        asset_paths.each do |path|
-          Rails.application.class.assets.prepend_path(path)
-        end
-      }
-
-    end
-
   end
 end
--- lib/spree/spree_landlord/tenant_helpers.rb
+++ lib/spree/spree_landlord/tenant_helpers.rb
@@ -5,6 +5,8 @@ module Spree
 
       included do
         before_filter :set_current_tenant
+        before_filter :add_tenant_view_path
+        before_filter :add_asset_paths
       end
 
       def set_current_tenant
@@ -12,6 +14,23 @@ module Spree
         tenant = finder.find_target_tenant(request)
         Spree::Tenant.set_current_tenant(tenant)
       end
+
+      def add_tenant_view_path
+        tenant = Tenant.current_tenant
+        path   = "app/tenants/#{tenant.shortname}/views"
+        prepend_view_path(path)
+      end
+
+      def add_asset_paths
+        tenant = Tenant.current_tenant
+        asset_paths = ["app/tenants/#{tenant.shortname}/assets/images",
+                       "app/tenants/#{tenant.shortname}/assets/stylesheets",
+                       "app/tenants/#{tenant.shortname}/assets/javascripts"]
+        asset_paths.each do |path|
+          Rails.application.class.assets.prepend_path(path)
+        end
+      end
+
     end
   end
 end
