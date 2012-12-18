c6afcceaad6c4fb5e8f9d725358ca282377cef81
M. Scott Ford <scott@mscottford.com>
2012-12-18 13:30:50 -0500

    Ensures that empty stores load with errors

---
+++ spec/api/empty_tenant_stores_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'empty tenant stores' do
+  let!(:master_tenant) { Spree::Tenant.master }
+  let!(:other_tenant) { FactoryGirl.create(:tenant) }
+
+  describe 'visiting empty master tenant' do
+    it 'reports no products found' do
+      get "http://#{master_tenant.domain}"
+      response.body.should include('No products found')
+    end
+  end
+
+  describe 'visiting empty non-master tenant' do
+    it 'reports no products found' do
+      get "http://#{other_tenant.domain}"
+      response.body.should include('No products found')
+    end
+  end
+end
