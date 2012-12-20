9ed9a22efcb7be05328704742e66ab33e0a4eaff
M. Scott Ford <scott@mscottford.com>
2012-12-20 16:46:38 -0500

    Refactors bootstrap specs to use AUTO_ACCEPT env variable

--- spec/acceptance/bootstrap_spec.rb
+++ spec/acceptance/bootstrap_spec.rb
@@ -5,20 +5,14 @@ describe 'bootstrap' do
   describe 'a spree site with landlord installed' do
     context 'without a database' do
       it 'runs without errors' do
-        commands = "y
-        y
-        "
-        output, status = Open3.capture2e('cd spec/dummy && bundle && bundle exec rake db:drop && bundle exec rake db:bootstrap', :stdin_data => commands)
+        output, status = Open3.capture2e('export AUTO_ACCEPT=true && cd spec/dummy && bundle && bundle exec rake db:drop && bundle exec rake db:bootstrap')
         status.should be_success
       end
     end
 
     context 'with a database' do
       it 'runs without errors' do
-        commands = "y
-        y
-        "
-        output, status = Open3.capture2e('cd spec/dummy && bundle && bundle exec rake db:drop && bundle exec rake db:migrate && bundle exec rake db:bootstrap', :stdin_data => commands)
+        output, status = Open3.capture2e('export AUTO_ACCEPT=true && cd spec/dummy && bundle && bundle exec rake db:drop && bundle exec rake db:migrate && bundle exec rake db:bootstrap')
         status.should be_success
       end
     end
