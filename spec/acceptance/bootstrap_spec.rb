a0fe6c6b0b8daded9ebd8623b1c949678a8bccdb
M. Scott Ford <scott@mscottford.com>
2012-12-20 17:34:42 -0500

    Refactors shell command execution to use rspec matcher
    This was done so that failed command output could be included in the
    test failure message.

--- spec/acceptance/bootstrap_spec.rb
+++ spec/acceptance/bootstrap_spec.rb
@@ -5,15 +5,15 @@ describe 'bootstrap' do
   describe 'a spree site with landlord installed' do
     context 'without a database' do
       it 'runs without errors' do
-        output, status = Open3.capture2e('export AUTO_ACCEPT=true && cd spec/dummy && bundle && bundle exec rake db:drop && bundle exec rake db:bootstrap')
-        status.should be_success
+        shell_command = 'export AUTO_ACCEPT=true && cd spec/dummy && bundle && bundle exec rake db:drop && bundle exec rake db:bootstrap'
+        shell_command.should run_without_errors
       end
     end
 
     context 'with a database' do
       it 'runs without errors' do
-        output, status = Open3.capture2e('export AUTO_ACCEPT=true && cd spec/dummy && bundle && bundle exec rake db:drop && bundle exec rake db:migrate && bundle exec rake db:bootstrap')
-        status.should be_success
+        shell_command = 'export AUTO_ACCEPT=true && cd spec/dummy && bundle && bundle exec rake db:drop && bundle exec rake db:migrate && bundle exec rake db:bootstrap'
+        shell_command.should run_without_errors
       end
     end
   end
