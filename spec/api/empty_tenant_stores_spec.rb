a2cede16e7641b1e8bad2f7a340078813afb619a
M. Scott Ford <scott@mscottford.com>
2013-01-03 16:27:50 -0500

    Deletes product data before running empty store examples

--- spec/api/empty_tenant_stores_spec.rb
+++ spec/api/empty_tenant_stores_spec.rb
@@ -4,6 +4,10 @@ describe 'empty tenant stores' do
   let!(:master_tenant) { Spree::Tenant.master }
   let!(:other_tenant) { FactoryGirl.create(:tenant) }
 
+  before {
+    Spree::Product.unscoped.destroy_all
+  }
+
   describe 'visiting empty master tenant' do
     it 'reports no products found' do
       get "http://#{master_tenant.domain}"
