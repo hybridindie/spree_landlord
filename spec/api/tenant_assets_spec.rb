8f14b5451ca2537162ffb2e3bface7d55b9be9ae
M. Scott Ford <scott@mscottford.com>
2013-01-09 10:45:52 -0500

    Adds failing specs for requiring files from css manifests
    I need to put this work on hold for a little bit, but someone else
    may be able to pick up where I've left off.
    
    Problems to solve:
    * It looks like vendor directories from the spree_core gem are not
      getting added to the tenant asset path. I suspect an issue with
      `Spree::SpreeLandlord::Assets::Bootstrap#run on lines 19-22
    * Need to figure out a reliable way to verify that scss has been
      compiled correctly. The formatting does not seem to be preserved
      which will make those specs brittle. It may be useful to use a
      css parser to compare the content in a way that ignores formatting
      and comments.

--- spec/api/tenant_assets_spec.rb
+++ spec/api/tenant_assets_spec.rb
@@ -17,36 +17,62 @@ describe 'tenant assets' do
     Spree::Tenant.create!(:shortname => 'apple', :domain => 'apple.com')
   end
 
-  it 'renders simple non-tenant asset' do
-    css = read_test_app_fixture_file('app/assets/stylesheets/test.css')
+  describe 'file retrieval' do
+    it 'renders simple non-tenant asset' do
+      css = read_test_app_fixture_file('app/assets/stylesheets/test.css')
 
-    get 'http://example.com/assets/test.css'
+      get 'http://example.com/assets/test.css'
 
-    response.body.should == css
-  end
+      response.body.should == css
+    end
 
-  it 'renders simple tenant asset' do
-    css = read_test_app_fixture_file('app/tenants/apple/assets/stylesheets/test.css')
+    it 'renders simple tenant asset' do
+      css = read_test_app_fixture_file('app/tenants/apple/assets/stylesheets/test.css')
 
-    get 'http://example.com/tenants/apple/assets/test.css'
+      get 'http://example.com/tenants/apple/assets/test.css'
 
-    response.body.should == css
-  end
+      response.body.should == css
+    end
 
-  it 'falls back to app asset directory from example tenant' do
-    css = read_test_app_fixture_file('app/assets/stylesheets/app.css')
+    it 'falls back to app asset directory from example tenant' do
+      css = read_test_app_fixture_file('app/assets/stylesheets/app.css')
 
-    get 'http://example.com/tenants/example/assets/app.css'
+      get 'http://example.com/tenants/example/assets/app.css'
 
-    response.body.should == css
-  end
+      response.body.should == css
+    end
 
-  it 'falls back to app asset directory from apple tenant' do
-    css = read_test_app_fixture_file('app/assets/stylesheets/app.css')
+    it 'falls back to app asset directory from apple tenant' do
+      css = read_test_app_fixture_file('app/assets/stylesheets/app.css')
 
-    get 'http://example.com/tenants/apple/assets/app.css'
+      get 'http://example.com/tenants/apple/assets/app.css'
 
-    response.body.should == css
+      response.body.should == css
+    end
+
+    context 'with scss require' do
+      it 'searches for required file in tenant tree' do
+        css = read_test_app_fixture_file('app/tenants/apple/assets/stylesheets/tenant_colors.css.scss')
+
+        get 'http://example.com/tenants/apple/assets/require_from_tenant.css'
+
+        response.body.should == css
+      end
+
+      it 'searches for required file in app tree' do
+        css = read_test_app_fixture_file('app/assets/stylesheets/app_colors.css.scss')
+
+        get 'http://example.com/tenants/apple/assets/require_from_app.css'
+
+        response.body.should == css
+      end
+
+      it 'searches for file from included gem' do
+        get 'http://example.com/tenants/apple/assets/require_html5reset.css'
+
+        response.body.should_not include('Error compiling CSS asset')
+      end
+    end
   end
 
   describe 'helper methods' do
