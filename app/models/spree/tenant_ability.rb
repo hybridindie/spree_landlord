fc284a12d93dba1032bf340774ab7c9aec9c23e4
M. Scott Ford <scott@mscottford.com>
2013-01-10 17:27:13 -0500

    Adds additional security for super admins

--- app/models/spree/tenant_ability.rb
+++ app/models/spree/tenant_ability.rb
@@ -9,6 +9,16 @@ module Spree
       else
         cannot :grant_super_admin, Spree::User
       end
+
+      cannot do |action, subject_class, subject|
+        if subject_class == Spree::User
+          if [:manage, :admin, :edit, :update, :delete].include?(action)
+            if subject.super_admin?
+              !user.super_admin
+            end
+          end
+        end
+      end
     end
 
     Spree::Ability.register_ability(Spree::TenantAbility)
